61c3eecbb454e34aaf5805bdc938c1aee3d3d5ca
Swati-Pandey-Voya <swati.pandey@voya.com>
2025-02-10 13:51:13 +0530

    Update Home.tsx

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -6,22 +6,46 @@ import { IHeaderCommandBarItem } from "azure-devops-ui/HeaderCommandBar";
 import * as SDK from "azure-devops-extension-sdk";
 import "azure-devops-ui/Core/override.css";
 import { showRootComponent } from "../Common";
+//import "./iconFont.css";
+import TableExample from "./Pipeline";
+//import  AzureDevopsPipelines from './getPipelineData';
+import { SurfaceBackground, SurfaceContext } from "azure-devops-ui/Surface";
+import { CommonServiceIds, getClient, IHostPageLayoutService } from "azure-devops-extension-api";
+import { BuildDefinition, BuildRestClient } from "azure-devops-extension-api/Build";
 
 
 export class HomePage extends React.Component<{}, {}> {
     public componentDidMount() {
-        SDK.init();
+         SDK.init();
+        /*SDK.register("sample-build-menu", () => {
+            return {
+                execute: async (context: BuildDefinition) => {
+                    console.log("BuildDefinition==");
+                    const result = await getClient(BuildRestClient).getDefinition(context.project.id, context.id, undefined, undefined, undefined, true);
+                    console.log("result=="+result);
+                    const dialogSvc = await SDK.getService<IHostPageLayoutService>(CommonServiceIds.HostPageLayoutService);
+                    dialogSvc.openMessageDialog(`Fetched build definition ${result.name}. Latest build: ${JSON.stringify(result.latestBuild)}`, { showCancel: false });
+                }
+            }
+        });*/
     }
-
+    
     public render() : JSX.Element {
-        return (
+        /*return (
             <div style={{width:"100%"}}>
                 <Header title="My first extension" titleSize={TitleSize.Large}/>
                 <div className="page-content flex-grow" style={{marginTop: "20px", marginLeft: "20px", marginRight: "20px"}}>
                     <Card>Page content</Card>
+                    <TableExample />
                 </div>
             </div>
-        );
+        );*/
+        return (
+        <SurfaceContext.Provider value={{ background: SurfaceBackground.neutral }}>
+            <Card>My First Extension For Devops Pipeline</Card>
+            <TableExample />
+        </SurfaceContext.Provider>
+        )
     }
 }
 export default HomePage;
